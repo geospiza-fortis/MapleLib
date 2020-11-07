e5fb44af625287ec8b2bc77133d1a4bf9313e2bb
geospiza <geospiza@protonmail.com>
2020-11-07 13:15:47 -0800

    Add project for serialization tests

---
+++ Tests/WzLibTests/WzSerializerTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MapleLib.WzLib.Serialization;
+
+namespace WzLibTests
+{
+    [TestClass]
+    public class WzSerializerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
